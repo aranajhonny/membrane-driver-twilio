1577158df044542a1f7902c02d55c43c784c662d
Me <me@example.com>
2018-06-27 19:45:02 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ export const MessageCollection = {
     return apiGet('get', `/Accounts/${ACCOUNT_SID}/Messages/${args.sid}.json`);
   },
   async sendSms({ args }) {
-    return apiPost(`/Accounts/${ACCOUNT_SID}/Messages/${args.sid}.json`, {
+    return apiPost(`/Accounts/${ACCOUNT_SID}/Messages.jsonn`, {
       From: args.from,
       To: args.to,
       Body: args.body,
